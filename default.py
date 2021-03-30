e5c755bb6ef8cb535b3fd569bc96bb8f38fb9b8e
Arne <arne@familie-schild.info>
2021-03-30 16:45:36 +0200

    More log entries

--- default.py
+++ default.py
@@ -22,7 +22,7 @@ class UpdateService():
     
     def start(self):
         
-        xbmc.log("scan_exec.py : Starting Kodi MyPicsDB2 Update Service", xbmc.LOGINFO)
+        xbmc.log("Starting Kodi MyPicsDB2 Update Service", xbmc.LOGINFO)
         # sleep at the beginning "startup_sleep" seconds.
         startup_sleep = abs(int(__settings__.getSetting( "STARTUP_SLEEP" )))
 
@@ -32,15 +32,16 @@ class UpdateService():
                 KODIMONITOR = xbmc.Monitor()
                 KODIMONITOR.waitForAbort( 5 )
                 self.slept = self.slept + 5
+                xbmc.log("Kodi MyPicsDB2 Update Service wait loop. Waiting %s"%self.slept, xbmc.LOGDEBUG)
 
         self.slept = 0
-
+        xbmc.log("Kodi MyPicsDB2 Update Service after wait loop", xbmc.LOGDEBUG)
         while not xbmc.Monitor().abortRequested():
 
             try:
                 # then wait sleep_period (value of var is in seconds)
                 sleep_period = abs(int(__settings__.getSetting( "SLEEP_PERIOD" )) * 60)
-
+                xbmc.log("Kodi MyPicsDB2 Update Service after wait loop. sleep_period %s"%sleep_period, xbmc.LOGDEBUG)
                 if sleep_period > 0 and self.slept >= sleep_period:
                     self.slept = 0
                     script = "%s,--refresh"% join( __homepath__, "..", "plugin.image.mypicsdb2", "scanpath.py")
@@ -49,7 +50,7 @@ class UpdateService():
                 # sleep at least 5 minutes
                 if sleep_period < 5:
                     sleep_period = 5
-
+                xbmc.log("Kodi MyPicsDB2 Update Service after wait loop. Waiting %s"%self.slept, xbmc.LOGDEBUG)
                 # sleep 1 second
                 xbmc.sleep(1000)    
                 self.slept = self.slept + 1
